b55baae7f37d91492aa8548cff3c9d56c8776b21
Nguyễn Duy Ngọc <92100963+Dautay2402@users.noreply.github.com>
2023-07-02 20:09:38 +0700

    Second

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ from fastapi.staticfiles import StaticFiles
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import TruncatedSVD
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory=r"D:/UIT/Năm 2/Kỳ 4/Tính toán đa phương tiện/Document similarity/static"), name="static")
 
 def bow_similarity(text1, text2):
     corpus = [text1, text2]
-    vectorizer = CountVectorizer(min_df=0, binary=True)
+    vectorizer = CountVectorizer()
     vectorized_corpus = vectorizer.fit_transform(corpus)
     similarity_matrix = cosine_similarity(vectorized_corpus)
     similarity = similarity_matrix[0, 1]
@@ -30,7 +31,7 @@ async def compare_texts_bow(doc1: str = Form(...), doc2: str = Form(...)) -> dic
 
 def tfidf_similarity(text1, text2):
     corpus = [text1, text2]
-    vectorizer = TfidfVectorizer(min_df=0)
+    vectorizer = TfidfVectorizer()
     vectorized_corpus = vectorizer.fit_transform(corpus)
     similarity_matrix = cosine_similarity(vectorized_corpus)
     similarity = similarity_matrix[0, 1]
@@ -45,6 +46,31 @@ async def compare_form_tfidf():
 @app.post("/compare_tfidf")
 async def compare_texts_tfidf(doc1: str = Form(...), doc2: str = Form(...)) -> dict:
     similarity = tfidf_similarity(doc1, doc2)
+    return {"similarity": round(similarity, 4)}
+
+def lsa_similarity(text1, text2):
+    corpus = [text1, text2]
+    vectorizer = TfidfVectorizer()
+    vectorized_corpus = vectorizer.fit_transform(corpus)
+
+    # Apply SVD
+    lsa = TruncatedSVD()
+    lsa_corpus = lsa.fit_transform(vectorized_corpus)
+
+    # Use cosine similarity on the LSA-transformed vectors
+    similarity_matrix = cosine_similarity(lsa_corpus)
+    similarity = similarity_matrix[0, 1]
+    return similarity
+
+@app.get("/LSA", response_class=HTMLResponse)
+async def compare_form_lsa():
+    with open('static/Semantic/LSA/LSA.html', 'r') as f:
+        content = f.read()
+    return content
+
+@app.post("/compare_lsa")
+async def compare_texts_lsa(doc1: str = Form(...), doc2: str = Form(...)) -> dict:
+    similarity = lsa_similarity(doc1, doc2)
     return {"similarity": round(similarity, 4)}
 
 if __name__ == "__main__":
